f7b42a87618c0fa1cb9c429f02df54329750773f
stevekozel <steve.kozel@gmail.com>
2014-04-01 22:02:24 -0500

    Update Personality_Charts_V1_edgeActions.js

--- Personality_Charts_V1_edgeActions.js
+++ Personality_Charts_V1_edgeActions.js
@@ -1,6 +1,7 @@
+
 (function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
 //Edge symbol: 'stage'
-(function(symbolName){Symbol.bindElementAction(compId,symbolName,"document","compositionReady",function(sym,e){$('Stage').css({"background-image":"url('http://personality.kosekozel.com/images/seamlesstexture6_1200.png')"});});
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"document","compositionReady",function(sym,e){$('Stage').css({"background-image":"url('personality.kosekozel.com/images/seamlesstexture6_1200.png')"});});
 //Edge binding end
 Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
 //Edge binding end
